4363e8361aecd30db0e26b9d9f368d97d9bacbf8
Arom <juandiegohuze@hotmail.com>
2024-01-05 21:16:28 -0500

    Se agregó el activity Register

---
+++ app/src/main/java/com/example/lectoya/Login.kt
@@ -0,0 +1,21 @@
+package com.example.lectoya
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class Login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        val button = findViewById<Button>(R.id.btnCrearcuenta)
+
+        button.setOnClickListener {
+            val intent = Intent(this, Register::class.java)
+            startActivity(intent)
+        }
+
+    }
+}
